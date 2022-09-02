70ec3c38f122d7ffb339327f986815d8c1a3ccfa
jyr <jean-yves.rouseau@orange.fr>
2022-09-02 14:22:54 +0200

    etape controller

---
+++ src/controllers/EtapesController.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from "express";
+import { CrudController } from "./CrudControler";
+import { etapes } from "../models/etapes";
+
+export class EtapesController extends CrudController {
+
+    public read(req: Request, res: Response): void {
+        etapes.findAll().then(etapes => res.json(etapes));
+    }
+    public create(req: Request, res: Response): void {
+        etapes.create(req.body).then(etape => res.json(etape));
+        console.log(req.body);
+        res.json("ok");
+    }
+    public update(req: Request, res: Response): void {
+        etapes.update(req.body, { 'where': { id: req.params.id } }).then(etape => res.json(etape));
+        res.json("ok");
+
+    }
+    public delete(req: Request, res: Response): void {
+        etapes.destroy({ 'where': { id: req.params.id } });
+        res.json("c'est fait");
+    }
+}
