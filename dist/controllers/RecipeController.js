5cef02bf2a480fca621b3fa96d6faa5f2720780c
jyr <jean-yves.rouseau@orange.fr>
2022-09-07 21:00:36 +0200

    open api

--- dist/controllers/RecipeController.js
+++ dist/controllers/RecipeController.js
@@ -4,30 +4,26 @@ exports.RecipeController = void 0;
 const CrudControler_1 = require("./CrudControler");
 const recipe_1 = require("../models/recipe");
 const saisons_1 = require("../models/saisons");
-const images_1 = require("../models/images");
 const etapes_1 = require("../models/etapes");
 const ingredients_1 = require("../models/ingredients");
 const ingredients_rectettes_1 = require("../models/ingredients_rectettes");
 const plats_1 = require("../models/plats");
 class RecipeController extends CrudControler_1.CrudController {
     async read(req, res) {
-        let photos = await images_1.images.findAll();
         let sais = await saisons_1.saisons.findAll();
         let recettess = await recipe_1.recettes.findAll();
         let ingredient = await ingredients_1.ingredients.findAll();
         let plat = await plats_1.plats.findAll();
-        res.json({ saisons: [...sais], recettes: [...recettess], images: [...photos], element: [...ingredient], plats: [...plat], });
+        res.json({ saisons: [...sais], recettes: [...recettess], element: [...ingredient], plats: [...plat], });
     }
     async show(req, res) {
-        let photos = await images_1.images.findAll({ 'where': { id: req.params.id } });
         let etape = await etapes_1.etapes.findAll({ 'where': { recette_id: req.params.id } });
         let recette = await recipe_1.recettes.findAll({ 'where': { id: req.params.id } });
         let ingreRecet = await ingredients_rectettes_1.ingredients_recettes.findAll({ 'where': { recettes_id: req.params.id } });
-        res.json({ recettes: [...recette], etape: [...etape], images: [...photos], ingre_recet: [...ingreRecet], });
+        res.json({ recettes: [...recette], etape: [...etape], ingre_recet: [...ingreRecet], });
     }
     create(req, res) {
         recipe_1.recettes.create(req.body).then(recipes => res.json(recipes));
-        /*res.json("ok");*/
     }
     update(req, res) {
         recipe_1.recettes.update(req.body, { 'where': { id: req.params.id } }).then(recipes => res.json(recipes));
--- src/controllers/RecipeController.ts
+++ src/controllers/RecipeController.ts
@@ -3,7 +3,6 @@ import { Request, Response } from "express";
 import { CrudController } from "./CrudControler";
 import { recettes } from "../models/recipe";
 import { saisons } from "../models/saisons";
-import { images } from "../models/images";
 import { etapes } from "../models/etapes";
 import { ingredients } from "../models/ingredients";
 import { ingredients_recettes } from "../models/ingredients_rectettes";
@@ -12,25 +11,22 @@ import { plats } from "../models/plats";
 export class RecipeController extends CrudController {
 
     public async read(req: Request, res: Response) {
-        let photos = await images.findAll();
         let sais = await saisons.findAll();
         let recettess = await recettes.findAll();
         let ingredient = await ingredients.findAll();
         let plat = await plats.findAll();
-        res.json({ saisons: [...sais], recettes: [...recettess], images: [...photos], element: [...ingredient], plats: [...plat], });
+        res.json({ saisons: [...sais], recettes: [...recettess], element: [...ingredient], plats: [...plat], });
 
     }
     public async show(req: Request, res: Response) {
-        let photos = await images.findAll({ 'where': { id: req.params.id } });
         let etape = await etapes.findAll({ 'where': { recette_id: req.params.id } });
         let recette = await recettes.findAll({ 'where': { id: req.params.id } });
         let ingreRecet = await ingredients_recettes.findAll({ 'where': { recettes_id: req.params.id } });
-        res.json({ recettes: [...recette], etape: [...etape], images: [...photos], ingre_recet: [...ingreRecet], });
+        res.json({ recettes: [...recette], etape: [...etape], ingre_recet: [...ingreRecet], });
 
     }
     public create(req: Request, res: Response): void {
         recettes.create(req.body).then(recipes => res.json(recipes));
-        /*res.json("ok");*/
     }
     public update(req: Request, res: Response): void {
         recettes.update(req.body, { 'where': { id: req.params.id } }).then(recipes => res.json(recipes));
